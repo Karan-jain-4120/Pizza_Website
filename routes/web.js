04980ece0824ebbbe0f76fece786b9238e3d6705
Karan-jain-4120 <karanjain4120@gmail.com>
2022-07-13 16:48:30 +0530

    add orders table

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,14 @@
 const homeController =require('../app/http/controller/homeController')
 const authController =require('../app/http/controller/authController')
 const cartController =require('../app/http/controller/customers/cartController')
+const orderController =require('../app/http/controller/customers/orderController')
+const AdminOrderController =require('../app/http/controller/admin/orderController')
+
+//middleware 
+const auth = require('../app/http/middlewares/auth')
 const guest = require('../app/http/middlewares/guest')
+const admin = require('../app/http/middlewares/admin')
+
 function initRoutes(app){
     app.get('/',homeController().index)
     
@@ -14,6 +21,15 @@ function initRoutes(app){
 
     app.get('/cart',cartController().index)
     app.post('/update-cart',cartController().update)
+   
+    // Customers routes
+    app.post('/orders',auth,orderController().store)
+    app.get('/customers/orders',auth,orderController().index)
+
+    
+     // Admin routes
+     app.get('/admin/orders', admin, AdminOrderController().index)
+    // app.post('/admin/order/status', admin, statusController().update)
 }
 
 module.exports=initRoutes
